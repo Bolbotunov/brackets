16b27ee3de6dbb224f62ed1e4076b833ebe6ed50
Bolbotunov <100226065+Bolbotunov@users.noreply.github.com>
2023-01-24 22:11:41 +0300

    feat: add code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    if (str.length %2 !==0){
+    return false
+    }
+      bracketsConfig.forEach((num) => {
+        const connect = num.join('');
+        if(str.includes(connect)){
+          str = str.replace(connect, '')
+          return connect
+        }
+    });
+if (str.length !== 0){
+  return false
+} else{
+  return true
 }
+}
+
