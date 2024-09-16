45ce5ddd3b7e770b5c611908e4bb08fe87269264
Konstantin <100226065+Bolbotunov@users.noreply.github.com>
2024-09-16 13:22:31 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,18 +1,25 @@
 module.exports = function check(str, bracketsConfig) {
-    if (str.length %2 !==0){
-    return false
-    }
-      bracketsConfig.forEach((num) => {
-        const connect = num.join('');
-        if(str.includes(connect)){
-          str = str.replace(connect, '')
-          return connect
+    let arr = [];
+    str = str.split('');
+    let open = [];
+    let close = [];
+        for(let i = 0; i < bracketsConfig.length ; i += 1) {
+            open.push(bracketsConfig[i][0]);
+            close.push(bracketsConfig[i][1]);
         }
-    });
-if (str.length !== 0){
-  return false
-} else{
-  return true
-}
-}
+        for(let i = 0; i < str.length; i += 1) {
+    
+           if(open.includes(str[i])) {
+            if(close.includes(str[i]) && arr[arr.length - 1] === str[i]) {
+                arr.pop();
+            } else {
+                arr.push(str[i]);
+            }
 
+           } else {
+            let pair = open[close.indexOf(str[i])]
+            if(arr.length === 0 || pair !== arr.pop()) return false;
+           }
+        }
+        return arr.length === 0;
+    }
